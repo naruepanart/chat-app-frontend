5a011d58a9cd669d7d7bf15429097927b4e9b3da
Naruepanart Siangsanan <naruepanart1201@gmail.com>
2022-10-20 15:33:51 +0700

    *

--- components/ChatComponents.js
+++ components/ChatComponents.js
@@ -1,10 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 /* import useEffectOnce from "../hooks/useEffectOnce";
 import useIsMounted from "../hooks/useIsMounted"; */
+import styles from "./styles.module.css";
 
 const ChatComponents = ({ socket, name, room }) => {
   const [currentMessage, setCurrentMessage] = useState("");
   const [messageList, setMessageList] = useState([]);
+  const messagesColumnRef = useRef(null);
 
   const sendMessage = (e) => {
     e.preventDefault();
@@ -23,44 +25,53 @@ const ChatComponents = ({ socket, name, room }) => {
     socket.on("rec", (data) => {
       setMessageList([...messageList, { ...data, require: false }]);
     });
+    return () => {
+      socket.off("rec");
+    };
   }, [messageList, socket]);
 
-  /*  useEffect(() => {
-    isMounted.current = true;
-    if (isMounted.current) {
-      console.log("first");
-      socket.on("rec", (data) => {
-        setMessageList([...messageList, data]);
-      });
-    }
-    return () => {
-      isMounted.current = false; // unmounted
-    };
-  }, [messageList, socket]); */
+  useEffect(() => {
+    messagesColumnRef.current.scrollTop = messagesColumnRef.current.scrollHeight;
+  }, [messageList]);
 
   return (
     <div>
-      <input
-        type="text"
-        placeholder="Message"
-        value={currentMessage}
-        onChange={(e) => setCurrentMessage(e.target.value)}
-      />
-      <button onClick={(e) => sendMessage(e)}>Send Message</button>
-
-      {messageList.map((x, i) => (
-        <p key={i}>
-          {x.require ? (
-            <p style={{ backgroundColor: "blue" }}>
-              {x.name} : {x.message}
-            </p>
-          ) : (
-            <p style={{ backgroundColor: "#f5f5f5" }}>
-              {x.name} : {x.message}
-            </p>
-          )}
-        </p>
-      ))}
+      <div className={styles.messagesColumn} ref={messagesColumnRef}>
+        {messageList.map((x, i) => (
+          <p key={i}>
+            {x.require ? (
+              <p
+                style={{
+                  background: "linear-gradient(115deg, #62cff4, #2c67f2)",
+                  textAlign: "right",
+                  color: "#FFF",
+                  padding: "3em",
+                }}
+              >
+                {x.name} : {x.message}
+              </p>
+            ) : (
+              <p style={{ backgroundColor: "#F1F5F9", padding: "3em" }}>
+                {x.name} : {x.message}
+              </p>
+            )}
+          </p>
+        ))}
+      </div>
+      <div>
+        <input
+          className={styles.messageInput}
+          type="text"
+          placeholder="Type a message..."
+          value={currentMessage}
+          onChange={(e) => setCurrentMessage(e.target.value)}
+          onKeyPress={(e) => {
+            if (e.key === "Enter") {
+              sendMessage(e);
+            }
+          }}
+        />
+      </div>
     </div>
   );
 };
--- pages/[id].js
+++ pages/[id].js
@@ -3,7 +3,6 @@ import { io } from "socket.io-client";
 import { nanoid } from "nanoid";
 import { useRouter } from "next/router";
 import ChatComponents from "../components/ChatComponents";
-import useEffectOnce from "../hooks/useEffectOnce";
 
 const socket = io("http://localhost:3001", { transports: ["websocket"] });
 
@@ -13,14 +12,14 @@ const ChatID = () => {
   const [name, setName] = useState("");
 
   useEffect(() => {
+    if (id === "") return;
     socket.emit("join-room", { id });
     setName(nanoid());
   }, [id]);
 
   return (
-    <div>
+    <div style={{ margin: "0 auto", width: "720px" }}>
       <h1>Chat : {id}</h1>
-
       <ChatComponents socket={socket} name={name} room={id} />
     </div>
   );
